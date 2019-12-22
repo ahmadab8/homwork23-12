42929f919d1b5ec8a68b92b39dedf27cb13ae8bd
omarhmdea <omarhm@ac.sce.ac.il>
2019-12-22 15:48:33 +0200

    Class WorkingStudent

---
+++ WorkingStudent.h
@@ -0,0 +1,15 @@
+
+#include  "Employee .h"
+#include "Student.h"
+
+class WorkingStudent :public Employee, public Student
+{
+	bool same_institute;
+public:
+	WorkingStudent(char* name, long id, int age, int averge_, char* institute_, float salary, bool same_institute) :Employee(name, id, age, salary), Student(name, id, age, averge_,institute_), Person(name, id, age)
+	{
+
+	}
+
+
+};
